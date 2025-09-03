47d7a9de9ff3f1f7154f7a17c6365b0d41af4724
Bryan Descours <bryan.delianov@gmail.com>
2025-09-03 12:17:25 +0200

    Réalisation de l'exercice 1

--- Exercice1/Program.cs
+++ Exercice1/Program.cs
@@ -6,3 +6,14 @@
 
 string nom = "Descours";
 Console.WriteLine(nom);
+
+// Déclarer une variable prix et une variable quantité de type entier. Calculer et afficher prix * quantité
+
+double prix = 21.99;
+Console.WriteLine(prix);
+
+int quantite = 3;
+Console.WriteLine(quantite);
+
+int total = prix * quantite;
+Console.WriteLine(total);
