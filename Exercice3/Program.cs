679742524038d019fe533738c62c4f51801b40f9
Bryan Descours <bryan.delianov@gmail.com>
2025-09-03 15:26:57 +0200

    Mise à jour de l'exercice 3

--- Exercice3/Program.cs
+++ Exercice3/Program.cs
@@ -6,12 +6,12 @@
 int a = 45;
 int b = 5;
 
-// int somme = a + b;
-// int difference = a - b;
-// int produit = a * b;
-// int quotient = a / b;
+int somme = a + b;
+int difference = a - b;
+int produit = a * b;
+int quotient = a / b;
 
-Console.WriteLine(a + " + " + b + " = " + a + b);
-Console.WriteLine(a - " - " + b + " = " + a - b);
-Console.WriteLine(a * " * " + b + " = " + a * b);
-Console.WriteLine(a / " / " + b + " = " + a / b);
+Console.WriteLine(a + " + " + b + " = " + somme);
+Console.WriteLine(a + " - " + b + " = " + difference);
+Console.WriteLine(a + " * " + b + " = " + produit);
+Console.WriteLine(a + " / " + b + " = " + quotient);
