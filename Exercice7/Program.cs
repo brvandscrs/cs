d9306601dfb49d0189a90e77e04eb73d891451ab
Bryan Descours <bryan.delianov@gmail.com>
2025-09-03 15:50:14 +0200

    Réalisation de l'exercice 7

--- Exercice7/Program.cs
+++ Exercice7/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Exercice 7");
+
+// Demander un nombre entre 1 et 7, puis utiliser un switch pour afficher le jour correspondant (1 = Lundi, 2 = Mardi, etc.)
+
+int jour = int.Parse(Console.ReadLine());
+
+switch (jour)
+{
+    case == 1:
+        Console.WriteLine("Nous sommes lundi");
+        break;
+
+    case == 2:
+        Console.WriteLine("Nous sommes mardi");
+        break;
+
+    case == 3:
+        Console.WriteLine("Nous sommes mercredi");
+        break;
+
+    case == 4:
+        Console.WriteLine("Nous sommes jeudi");
+
+    case == 5:
+        Console.WriteLine("Nous sommes vendredi");
+        break;
+
+    case == 6:
+        Console.WriteLine("Nous sommes samedi");
+        break;
+
+    case == 7:
+        Console.WriteLine("Nous sommes dimanche");
+        break;
+
+    default:
+        Console.WriteLine("Numéro de jour incorrect. Recommencez.");
+        break;
+}
