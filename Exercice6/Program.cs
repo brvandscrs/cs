4f59f0b8b9d624ca12d17d51aabaf9f5a489d38e
Bryan Descours <bryan.delianov@gmail.com>
2025-09-03 15:41:25 +0200

    Réalisation de l'exercice 6

--- Exercice6/Program.cs
+++ Exercice6/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Exercice 6");
+
+// Demander deux nombres et afficher lequel est le plus grand, ou si ce sont les mêmes
+
+int nombre1 = int.Parse(Console.ReadLine());
+int nombre2 = int.Parse(Console.ReadLine());
+
+// int lePlusGrand = Math.Max(nombre1, nombre2);
+
+if (nombre1 != nombre2)
+    Console.WriteLine("Le plus grand nombre des deux est " + Math.Max(nombre1, nombre2) + ".");
+else
+    Console.WriteLine("Les deux nombres sont exactement les mêmes : " + nombre1 + ".");
