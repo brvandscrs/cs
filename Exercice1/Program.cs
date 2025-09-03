af67a1cfd0800ab3f44d2795f77e154e8d90b2af
Bryan Descours <bryan.delianov@gmail.com>
2025-09-03 12:08:56 +0200

    Ajout exercice 1

---
+++ Exercice1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Exercice 1");
+
+// Enoncé
+// Créer une variable de type string et l'initialiser avec une chaîne de caractères (votre nom)
+// Afficher la variable dans la console
+
+string nom = "Descours";
+Console.WriteLine(nom);
