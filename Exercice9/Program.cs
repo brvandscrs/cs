1d1c35221c08d3978788bc940a7b2a1a052a6e06
Bryan Descours <bryan.delianov@gmail.com>
2025-09-03 16:20:40 +0200

    Réalisation de l'exercice 9

--- Exercice9/Program.cs
+++ Exercice9/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Exercice 9");
+
+// Demander une note (entre 0 et 20). Utiliser un switch avec des plages (when) pour afficher :
+// [0 à 9] : "Insuffisant"
+// [10 à 13] : "Moyen"
+// [14 à 16] : "Bien"
+// [17 à 20] : "Excellent"
+
+int note = int.Parse(Console.ReadLine());
+
+switch (note)
+{
+    case int f when (f >= 0 & f <= 9):
+        Console.WriteLine("Insuffisant");
+        break;
+
+    case int f when (f >= 10 & f <= 13):
+        Console.WriteLine("Moyen");
+        break;
+
+    case int f when (f >= 14 & f <= 16):
+        Console.WriteLine("Bien");
+        break;
+
+    case int f when (f >= 17 & f <= 20):
+        Console.WriteLine("Excellent");
+        break;
+
+    default:
+        Console.WriteLine("Note non reconnue.Recommencez.");
+        break;
+}
