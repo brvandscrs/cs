3a510405a5e97ea70bf5c2062ca6e5f49d26326e
Bryan Descours <bryan.delianov@gmail.com>
2025-09-03 11:45:27 +0200

    commit initial

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,4 @@
+﻿// Console : Objet de Framework .NET qui représente la console (de VSCode)
+// WriteLine : Méthode de Console qui affiche une chaîne de caractères dans la console (avec passage à la ligne)
+Console.WriteLine("Hello, World!");
+Console.Clear(); // Efface le contenu de la console
---
+++ Variables/Program.cs
@@ -0,0 +1,28 @@
+﻿// Généralités
+// C# est case sensitive (console <> Console)
+// ; à la fin de chaque instruction
+
+// Déclaration d'une variable
+// Type, Nom, (= Valeur)
+
+// Type string : chaîne de caractères
+string message = "Hello, Wordl!";
+Console.WriteLine(message);
+
+// Type int : entier
+int nombre = 42;
+Console.WriteLine(nombre);
+
+double prix = 19.99;
+Console.WriteLine(prix);
+
+// int // Type entier
+// double // Type nombre à virgule flottante
+// string // Type chaîne de caractères
+// bool // Type booléen (true/false)
+// char // Type caractère (ex: 'A')
+// float // Type nombre à virgule flottante (simple précision)
+// long // Type entier (64 bits)
+// decimal // Type nombre décimal (précision élevée, utilisé pour les calculs financiers)
+// byte // Type entier (0 à 255)
+// short // Type entier (16 bits)
