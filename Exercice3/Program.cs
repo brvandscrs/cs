b8033e496c5c7a5e690d7e0ccc465f15803a7158
Bryan Descours <bryan.delianov@gmail.com>
2025-09-03 14:54:22 +0200

    Ajout des exercices 3 à 10, exécution de l'exercice 3

---
+++ Exercice3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Exercice 3");
+
+// Déclarer deux nombres entiers a et b
+// Afficher la somme, la différence, le produit et le quotient de a et b
+
+int a = 45;
+int b = 5;
+
+Console.WriteLine(a + b);
+Console.WriteLine(a - b);
+Console.WriteLine(a * b);
+Console.WriteLine(a / b);
