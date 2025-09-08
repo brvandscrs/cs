4f2c6227412b8588a8e808ce774cd76720495177
Bryan Descours <bryan.delianov@gmail.com>
2025-09-08 11:16:32 +0200

    ajout du dossier boucles

---
+++ boucles/Program.cs
@@ -0,0 +1,6 @@
+﻿// for : quant on connait à l'avance le nombre d'itérations
+
+// while : quand on ne connait pas à l'avance le nombre d'itérations
+// do ... while : variante de while qui exécute au moins une fois le code de la boucle
+
+// foreach : pour itérer sur les collections
