922adcb5f2f7af76e94efd61966eb4282511f142
Bryan Descours <bryan.delianov@gmail.com>
2025-09-08 15:40:57 +0200

    Ajout de Fonctions et ExercicesFonctions

---
+++ ExercicesFonctions/Program.cs
@@ -0,0 +1,42 @@
+﻿// Exercice 1
+
+Console.WriteLine("Exercice 1");
+
+// Ecrire une fonction qui calcule le carré d'un nombre
+
+int Carre(int nombre)
+{
+    return nombre * *2;
+}
+
+// Test 3 fois
+
+int nombreCarre;
+
+for (int i = 1; i <= 3; i++)
+{
+    nombreCarre = Convert.ToInt32(Console.ReadLine());
+    Carre(nombreCarre);
+}
+
+// Exercice 2
+
+Console.WriteLine("Exercice 3");
+
+// Eccrire une fonction qui affiche "Bonjour" dans la console
+
+string MessageBonjour()
+{
+    return "Bonjour";
+}
+
+// Exercice 3
+
+Console.WriteLine("Exercice 3");
+
+// Ecrire une fonction qui retourne le plus grand de deux entiers
+
+int PlusGrand(int entier1, int entier2)
+{
+    return Math.Max(entier1, entier2);
+}
---
+++ Fonctions/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Fonctions");
+
+// Modèle abstrait d'une fonction
+// typeDeRetour NomDeLaFonction
+// {
+//     Instriction...
+//     return valeurDeRetour
+// }
+
+// Déclaration de la fonction Addition
+int Addition(int nbEntier1, int nbEntier2)
+{
+    return nbEntier1 + nbEntier2;
+}
+
+int total = Addition(2, 3);
+
+Console.WriteLine(total);
