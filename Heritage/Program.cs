ece0d918baa348e1fc997ff1af4aef50844ad37b
Bryan Descours <bryan.delianov@gmail.com>
2025-09-22 11:14:58 +0200

    Mise à jour du projet Heritage

--- Heritage/Program.cs
+++ Heritage/Program.cs
@@ -3,20 +3,31 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Héritage C#");
+
         Utilisateur user = new Utilisateur();
         user.Nom = "Descours";
         user.Prenom = "Bryan";
         user.Afficher();
-    }
-}
 
-public class Utilisateur
-{
-    public string Nom { get; set; }
-    public string Prenom { get; set; }
+        Utilisateur user = new Utilisateur("Bryan", "Descours");
+        user.Afficher();
 
-    public void Afficher()
-    {
-        Console.WriteLine($"{Nom} {Prenom}");
+        Client client = new Client();
+        client.Nom = "Dupont";
+        client.Prenom = "Jacques";
+        client.Adresse = "Place Jean Jaurès 84740 Velleron";
+        client.Afficher();
+
+        Client client = new Client("Jacques", "Dupont", "Place Jean Jaurès 84740 Velleron");
+        client.Afficher();
+
+        Employe employe = new Employe();
+        employe.Nom = "Martin";
+        employe.Prenom = "Paul";
+        employe.Poste = "Secrétaire";
+        employe.Afficher();
+
+        Employe employe = new Employe("Paul", "Martin", "Secrétaire");
+        employe.Afficher();
     }
 }
--- Heritage/Utilisateur.cs
+++ Heritage/Utilisateur.cs
@@ -1,17 +1,43 @@
 public class Utilisateur
 {
-    // Propriétés
+    public string Nom { get; set; }
+    public string Prenom { get; set; }
 
-    public Utilisateur(string Nom, string Prenom)
+    public Utilisateur()
     {
-        Nom = Nom;
-        Prenom = Prenom;
+        Nom = "";
+        Prenom = "";
     }
-
-    // Méthodes
 
     public void Afficher()
     {
-        Console.WriteLine($"Solde: {Solde}");
+        Console.WriteLine($"{Nom} {Prenom}");
     }
+}
+
+public class Administrateur : Utilisateur
+{
+
+}
+
+public class Client : Utilisateur
+{
+    public Client()
+    {
+        Adresse = "";
+    }
+
+    public string Adresse { get; set; }
+
+    public void Afficher()
+}
+
+public class Employe : Utilisateur
+{
+    public Employe()
+    {
+        Poste = "";
+    }
+
+    public string Poste { get; set; }
 }
